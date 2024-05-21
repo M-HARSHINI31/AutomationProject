5e1661f1af91f0268272bd383b5af66e470f8463
Akhilkeerthi98 <akhilkeerthidarling@gmail.com>
2024-05-21 18:27:13 +0530

    create,delete functions added

---
+++ src/main/java/com/cogmento/pages/TasksPage.java
@@ -0,0 +1,51 @@
+package com.cogmento.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class TasksPage extends BasePage{
+    public TasksPage(WebDriver driver) {
+        super(driver);
+    }
+
+    private final By taskTitle = By.xpath("//label[text()='Title']//following-sibling::div//input");
+    private final By taskDescription = By.xpath("//label[text()='Description']//following-sibling::textarea");
+
+//    private By taskAssignedTo = By.xpath("//label[text()='Assigned To']//following-sibling::div");
+//    private By taskAssignedVisible = By.xpath("//label[text()='Assigned To']//following-sibling::div//span[text()='Supriya Siriki']");
+
+
+
+    public void createTask(String title, String description) throws Exception {
+        clickOnCreateTask();
+        enterTaskDetails(title,description);
+        clickOnSave();
+        checkTaskHeader(title);
+    }
+
+    public void clickOnCreateTask () throws Exception {
+        createButton();
+    }
+
+    public void enterTaskDetails (String title, String description){
+        scriptAction.inputText(taskTitle, title);
+        scriptAction.inputText(taskDescription, description);
+//        scriptAction.clickElement(taskAssignedTo);
+//        scriptAction.clickElement(taskAssignedVisible);
+
+    }
+
+    public void clickOnSave () throws Exception {
+        saveButton();
+    }
+
+    public void checkTaskHeader (String title) throws Exception {
+        checkPageHeader(title);
+    }
+
+    public void clickOnDelete(String title, String popUpValue) throws Exception {
+        deleteRecord(title,popUpValue);
+    }
+
+
+}
